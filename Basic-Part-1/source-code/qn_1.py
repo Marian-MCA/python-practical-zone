6adf2c9d514413dc7680f18aeca60dd8f4d02013
Ajil Raju <ajilraju01@gmail.com>
2018-09-03 22:02:41 +0530

    qn_1 question source code test

---
+++ Basic-Part-1/source-code/qn_1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+print("Twinkle, twinkle, little star,\n\t\
+How I wonder what you are!\n\t\t\
+Up above the world so high,\n\t\t\
+Like a diamond in the sky.\n\
+Twinkle, twinkle, little star,\n\tHow I wonder what you are")
