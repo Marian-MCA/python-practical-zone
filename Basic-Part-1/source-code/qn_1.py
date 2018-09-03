2edbd0718d1f2af08e39e47da5bc8dbf831f09cf
Ajil Raju <ajilraju01@gmail.com>
2018-09-03 22:14:04 +0530

    Formatted qn_1 source

--- Basic-Part-1/source-code/qn_1.py
+++ Basic-Part-1/source-code/qn_1.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 print("Twinkle, twinkle, little star,\n\t\
-How I wonder what you are!\n\t\t\
-Up above the world so high,\n\t\t\
-Like a diamond in the sky.\n\
-Twinkle, twinkle, little star,\n\tHow I wonder what you are")
+	   How I wonder what you are!\n\t\t\
+	   Up above the world so high,\n\t\t\
+	   Like a diamond in the sky.\n\
+	   Twinkle, twinkle, little star,\n\t\
+	   How I wonder what you are")
