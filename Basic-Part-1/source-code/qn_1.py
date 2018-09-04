3e4b1a15b9d5219e49c1578e09512ecfd7cc4f77
gol-dy <goldytjoseph007@gmail.com>
2018-09-04 07:13:56 +0530

    cleared

--- Basic-Part-1/source-code/qn_1.py
+++ Basic-Part-1/source-code/qn_1.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-print("""Twinkle, twinkle, little star,
-	How I wonder what you are! 
-		Up above the world so high,   		
-		Like a diamond in the sky. 
-Twinkle, twinkle, little star, 
-How I wonder what you are""")
+print("""
+	Twinkle, twinkle, little star,
+		How I wonder what you are! 
+			Up above the world so high,   		
+			Like a diamond in the sky. 
+	Twinkle, twinkle, little star, 
+		How I wonder what you are.
+      """)
+	
